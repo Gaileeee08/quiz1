07f6eff3337dba1e9346811d26db5e6876f94c45
Justine Gail Valdez <44793489+Gaileeee08@users.noreply.github.com>
2025-03-14 11:03:40 +0000

    quiz 1 completed

---
+++ quiz1/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InventoryController;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('/inventory', InventoryController::class);
+
+// This tells the website to listen for a link like "hello/John" and grab the "John" part
+
+Route::get('hello/{name}', function($name) {
+
+    return 'hi ' . $name; // It will say "hi" followed by whatever name you typed (like "hi John")
+
+});
